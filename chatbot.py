a2cbb9d2c9bf40589c4fd03c87faf57cd39cdb4b
Hoai-An Phan <pnhoaian2904@gmail.com>
2023-06-24 00:19:24 -0700

    Add API call to openAI

--- chatbot.py
+++ chatbot.py
@@ -4,3 +4,19 @@ from streamlit_chat import message
 from decouple import config
 
 openai.api_key = config('OPENAI_API_KEY')
+
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stope=None,
+        temparature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+
+st.title('chatBot: Streamlit + openAI')
